d07c5a7fdb860e8f41fd0255d2d589306c04890d
Kyle <1396676171@qq.com>
2018-11-22 17:59:41 +0800

    Add files via upload

---
+++ Version-0.1/cs/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace K_Browser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.bookmark = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "www.google.com",
+            "www.baidu.com",
+            "www.bing.com",
+            "www.yandex.com"});
+            this.comboBox1.Location = new System.Drawing.Point(31, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(757, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBox1_KeyDown);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(1, 30);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(796, 417);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // bookmark
+            // 
+            this.bookmark.BackColor = System.Drawing.SystemColors.Menu;
+            this.bookmark.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bookmark.BackgroundImage")));
+            this.bookmark.FlatAppearance.BorderSize = 0;
+            this.bookmark.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bookmark.Location = new System.Drawing.Point(1, 3);
+            this.bookmark.Name = "bookmark";
+            this.bookmark.Size = new System.Drawing.Size(24, 21);
+            this.bookmark.TabIndex = 2;
+            this.bookmark.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bookmark);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.comboBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button bookmark;
+    }
+}
+
---
+++ Version-0.1/cs/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace K_Browser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            this.Text = "K-Browser";
+            //webBrowser1.Url = new Uri(@"https://www.google.com");
+            webBrowser1.Url = new Uri(@"https://www.cnblogs.com");
+        }
+
+        private void comboBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                webBrowser1.Navigate(comboBox1.Text);
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            webBrowser1.Height = this.Height;
+            webBrowser1.Width = this.Width - 18;
+            comboBox1.Width = this.Width - 49;
+        }
+    }
+}
