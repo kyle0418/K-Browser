53237a5f6622759d5fdb90842703291970880cb4
Kyle <1396676171@qq.com>
2018-12-08 13:55:46 +0800

    Add files via upload

---
+++ Version-0.2.1/cs/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KB_WebKit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webKitBrowser1.Navigate("http://www.baidu.com");
+        }
+    }
+}
